512e7664b0fae022cfb4f52def7b216315acb799
Wesley-wsl <wesleywestelley@gmail.com>
2022-12-28 20:15:50 -0300

    feat: implements notification pattern in customer and product

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,19 +1,71 @@
+// import ProductInterface from "./product.interface";
+
+// export class ProductB implements ProductInterface {
+//   private _id: string;
+//   private _name: string;
+//   private _price: number;
+
+//   constructor(id: string, name: string, price: number) {
+//     this._id = id;
+//     this._name = name;
+//     this._price = price;
+//     this.validate();
+//   }
+
+//   get id(): string {
+//     return this._id;
+//   }
+
+//   get name(): string {
+//     return this._name;
+//   }
+
+//   get price(): number {
+//     return this._price * 2;
+//   }
+
+//   changeName(name: string): void {
+//     this._name = name;
+//     this.validate();
+//   }
+
+//   changePrice(price: number): void {
+//     this._price = price;
+//     this.validate();
+//   }
+
+//   validate(): boolean {
+//     if (this._id.length === 0) {
+//       throw new Error("Id is required");
+//     }
+//     if (this._name.length === 0) {
+//       throw new Error("Name is required");
+//     }
+//     if (this._price < 0) {
+//       throw new Error("Price must be greater than zero");
+//     }
+//     return true;
+//   }
+// }
+
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import ProductInterface from "./product.interface";
 
-export class ProductB implements ProductInterface {
-  private _id: string;
+export class ProductB extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
-  }
 
-  get id(): string {
-    return this._id;
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   get name(): string {
@@ -36,13 +88,22 @@ export class ProductB implements ProductInterface {
 
   validate(): boolean {
     if (this._id.length === 0) {
-      throw new Error("Id is required");
+      this.notification.addError({
+        context: "productb",
+        message: "Id is required",
+      });
     }
     if (this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({
+        context: "productb",
+        message: "Name is required",
+      });
     }
     if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
+      this.notification.addError({
+        context: "productb",
+        message: "Price must be greater than zero",
+      });
     }
     return true;
   }
--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -19,6 +19,12 @@ describe("Product unit tests", () => {
     }).toThrowError("Price must be greater than zero");
   });
 
+  it("should throw error when name and id are empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("", "", -10);
+    }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
